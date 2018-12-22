dc0a827334eb7cd57dc242e8ce370ce25976d53b
Oct <43613676+idontwhatismyname@users.noreply.github.com>
2018-12-22 08:37:46 +0800

    Delete func.h

--- Matrix/LinearAlgebra/func.h
+++
@@ -1,8 +0,0 @@
-matrix *InitializeMatrix();
-void ReleaseMatrix(matrix *mat);
-matrix *ReadMatrix();
-database read();
-void PrintMatrix(const matrix* const mat);
-void Replacement(const database row1[],database row2[],const int col,const database multiple);
-void Interchange(database *row1,database *row2,const int col);
-void Scaling(database *row,const database scales,const int col);
